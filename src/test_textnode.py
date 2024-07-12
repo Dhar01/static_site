0f02badbef6a257156c2a80008b1f2971ee6b6e4
Loknath Dhar <loknath-dhar@pm.me>
2024-07-13 01:40:02 +0600

    refactored

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -13,30 +13,30 @@ from textnode import (
 
 class testTextNode(unittest.TestCase):
     def test_eq(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node1 = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_bold)
         self.assertEqual(node1, node2)
 
     def test_notEq(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "italic")
+        node1 = TextNode("This is a text node", text_type_bold)
+        node2 = TextNode("This is a text node", text_type_italic)
         self.assertNotEqual(node1, node2)
 
     def test_url(self):
-        node1 = TextNode("Testing url", "text", "https://google.com")
-        node2 = TextNode("Testing url", "text", "https://google.com")
+        node1 = TextNode("Testing url", text_type_text, "https://google.com")
+        node2 = TextNode("Testing url", text_type_text, "https://google.com")
         self.assertEqual(node1, node2)
 
     def test_noneUrl(self):
-        node1 = TextNode("This is a text node", "bold", None)
-        node2 = TextNode("This is a text node", "bold")
+        node1 = TextNode("This is a text node", text_type_bold, None)
+        node2 = TextNode("This is a text node", text_type_bold)
         self.assertEqual(node1, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", "text", "https://google.com")
+        node = TextNode("This is a text node", text_type_bold, "https://google.com")
 
         self.assertEqual(
-            "TextNode(This is a text node, text, https://google.com)",
+            "TextNode(This is a text node, bold, https://google.com)",
             repr(node)
         )
 
