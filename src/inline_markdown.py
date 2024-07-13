5e9a050d5bbd1a37f612a70341859366ee0adaf1
Loknath Dhar <loknathdhar66@gmail.com>
2024-07-14 01:27:07 +0600

    Adding image splitting codes and tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -47,7 +47,30 @@ def extract_markdown_links(text: str) -> tuple:
 def split_nodes_images(old_nodes: list[TextNode]) -> list[TextNode]:
     new_nodes = []
 
-    # write later
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+
+            new_nodes.append(TextNode(image[0], text_type_image, image[1]))
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
 
     return new_nodes
 
