e924ca71db3c537a22e5bb8d8edcde71a65e1b55
Loknath Dhar <loknath-dhar@pm.me>
2024-07-13 16:49:37 +0600

    skeleton for testing inline markdown

---
+++ src/test_inlineMarkdown.py
@@ -0,0 +1,16 @@
+import unittest
+from inline_markdown import split_nodes_delimiter
+
+from textnode import(
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_link,
+    text_type_image
+)
+
+
+class TestInlineMarkdown(unittest.TestCase):
+    pass
