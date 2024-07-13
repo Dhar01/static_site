867d53b4798547febb794419b91555116bed6b17
Loknath Dhar <loknath-dhar@pm.me>
2024-07-13 18:21:58 +0600

    adding tests for inline markdown

--- src/test_inlineMarkdown.py
+++ src/test_inlineMarkdown.py
@@ -11,6 +11,44 @@ from textnode import(
     text_type_image
 )
 
-
 class TestInlineMarkdown(unittest.TestCase):
-    pass
+    def test_split_codeblock(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word", text_type_text),
+            ]
+        )
+
+    def test_split_boldItalic(self):
+        node = TextNode("This is **bold** text with more *bolded*", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("bold", text_type_bold),
+                TextNode(" text with more ", text_type_text),
+                TextNode("bolded", text_type_italic),
+            ]
+        )
+
+    # def test_split_image(self):
+    #     node = TextNode("This is ![image](image.jpg)", text_type_text)
+    #     new_nodes = split_nodes_delimiter([node], "", text_type_image)
+    #     self.assertEqual(
+    #         new_nodes,
+    #         [
+    #             TextNode("This is ", text_type_text)
+    #         ]
+    #     )
+
+if __name__ == "__main__":
+    unittest.main()
