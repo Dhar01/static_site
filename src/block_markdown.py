f6b8a4da40d656bdb42dbe58a35b9d77c74087c7
Loknath Dhar <loknathdhar66@gmail.com>
2024-07-27 16:32:00 +0600

    adding block support and its testcase

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,13 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ulist = "unordered_list"
+block_type_olist = "ordered_list"
+
+
 def markdown_to_blocks(markdown) -> list:
+    """breaking into blocks"""
     blocks = []
 
     for block in markdown.split('\n\n'):
@@ -7,3 +16,47 @@ def markdown_to_blocks(markdown) -> list:
         blocks.append(block.strip())
 
     return blocks
+
+def block_to_block_type(md_block) -> str:
+    """inspect markdown and determine the type of the block"""
+    lines = md_block.split("\n")
+
+    if (
+        md_block.startswith("# ")
+        or md_block.startswith("## ")
+        or md_block.startswith("### ")
+        or md_block.startswith("#### ")
+        or md_block.startswith("##### ")
+        or md_block.startswith("###### ")
+    ):
+        return block_type_heading
+
+    elif (
+        len(lines) > 1
+        and lines[0].startswith("```")
+        and lines[-1].startswith("```")
+    ):
+        return block_type_code
+
+    elif md_block.startswith("> "):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+
+    elif md_block.startswith("* ") or md_block.startswith("- "):
+        for line in lines:
+            if not line.startswith("* ") or line.startswith("- "):
+                return block_type_paragraph
+        return block_type_ulist
+
+    elif md_block.startswith("1. "):
+        number = 1
+        for line in lines:
+            if not line.startswith(f"{number}. "):
+                return block_type_paragraph
+            number += 1
+        return block_type_olist
+
+    else:
+        return block_type_paragraph
--- src/test_blockmarkdown.py
+++ src/test_blockmarkdown.py
@@ -1,6 +1,16 @@
 import unittest
+from unittest import result
 
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_heading,
+    block_type_paragraph,
+    block_type_code,
+    block_type_quote,
+    block_type_olist,
+    block_type_ulist,
+)
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markToBlocks(self):
@@ -24,6 +34,25 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                 "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
             ],
         )
+
+    def test_blockToBlock(self):
+        result = block_to_block_type("hello there")
+        self.assertEqual(result, block_type_paragraph)
+
+        result = block_to_block_type("> a quote\n> more quote")
+        self.assertEqual(result, block_type_quote)
+
+        result = block_to_block_type("### This is a Heading")
+        self.assertEqual(result, block_type_heading)
+
+        result = block_to_block_type("```\npython_code = input()\n```")
+        self.assertEqual(result, block_type_code)
+
+        result = block_to_block_type("* some order")
+        self.assertEqual(result, block_type_ulist)
+
+        result = block_to_block_type("1. some unorder]\n2. another unorder")
+        self.assertEqual(result, block_type_olist)
 
 if __name__ == "__main__":
     unittest.main()
