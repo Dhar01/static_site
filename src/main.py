1ddee3a927418e59274e743ef859f97bc7c13698
Loknath Dhar <loknath-dhar@pm.me>
2024-08-11 16:14:53 +0600

    generating single pages

--- src/main.py
+++ src/main.py
@@ -1,34 +1,9 @@
-import os
-import shutil
-import logging
-from textnode import TextNode
-from genericpath import exists, isfile
-
-# logging
-logging.basicConfig(level=logging.INFO, format="%(message)s")
-
-def clear_directory(path: str) -> None:
-    if os.path.exists(path):
-        shutil.rmtree(path)
-        logging.info(f"Directory '{path}' cleaned!")
-    else:
-        os.mkdir(path)
-        logging.info(f"Directory '{path}' created!")
-
-def copy_statToPub_contents(src: str, dest: str) -> None:
-    if not os.path.exists(dest):
-        os.mkdir(dest)
-
-    for filename in os.listdir(src):
-        src_path = os.path.join(src, filename)
-        dest_path = os.path.join(dest, filename)
-
-        if os.path.isdir(src_path):
-            shutil.copytree(src_path, dest_path)
-            logging.info(f"Directory copied: {dest_path}")
-        else:
-            shutil.copy(src_path, dest_path)
-            logging.info(f"File copied: {dest_path}")
+from generate import (
+    clear_directory,
+    logging,
+    copy_statToPub_contents,
+    generate_page
+)
 
 def main():
     src = 'static'
@@ -42,8 +17,12 @@ def main():
 
     logging.info(f"All contents are copied from {src} to {dst}")
 
-    # node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    # print(node)
+    # generating page
+    from_path = 'content/index.md'
+    template_path = 'template.html'
+    dest_path = 'public/index.html'
+
+    generate_page(from_path, template_path, dest_path)
 
 if __name__ == "__main__":
     main()
